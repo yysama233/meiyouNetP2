1360915c1a36f51973c638230ab7e02fded045dd
Yang <yysama95@gmail.com>
2017-04-03 15:07:28 -0400

    fuck:

--- client/reldataSender.py
+++ client/reldataSender.py
@@ -4,8 +4,35 @@ from string import *
 import struct
 from time import time
 import string
+"""
+Author: Yang Yang, Mingjun Xie, Yufeng Wang
+Version 1.0
+
+Client side of the data transfer, which implements the function such as packet packing, packet unpacking,
+packet analysis and send/receive packet. The user is allowed to use command as followed to run the python file
+
+python reldataSender.py [ip adress]:[port number] [windowsize] to establish connection with server
+
+and type in command line
+
+tranform [file to be transferred] to send out the file pack.
+
+"""
 class Packet(object):
     def __init__(self,data,seqNum,ackNum,flags,mrws):
+        """Function fo initialize the packet
+
+        Args:
+            data: data to be included in the packet
+            seqNum: sequence number of the packet
+            ackNum: ackknowledge number of the packet
+            flags: Synflag, AckFlag and Finflag array
+            mrws: available window size
+
+        Returns:
+            null
+
+        """
         self.seq_num = seqNum
         self.data = data
         self.datalen = len(data)
@@ -18,6 +45,16 @@ class Packet(object):
         #self.chksum = self.makecheksum(self.data)
         self.formatString = "!HHHH???B"+ (str)(self.datalen) + "s"
     def makecheksum(self,data,datalen):
+        """Function to make the checksum for the packet
+
+        Args:
+            data: data to be included in the packet
+            datalen: the length of the data
+
+        Returns:
+            sum: checksum value of this packet
+
+        """
         sum = 0
         #print "check sum datalen ",datalen
         for i in range(datalen):
@@ -27,14 +64,44 @@ class Packet(object):
                 sum = (sum + 1) & (0x0000FFFF)
         return sum
     def setchksum(self,checksum):
+        """Function that sets the checksum for the packet
+
+        Args:
+            checksum: checksum
+
+        Returns:
+            null
+
+        """
         self.chksum = checksum
     def pack(self):
+        """Function that pack the packet
+
+        Args:
+            null
+
+        Returns:
+            struct: packed packet
+
+        """
         #print "pack mrws: ",self.mrws
         self.chksum = self.makecheksum(self.data,self.datalen)
         return struct.pack(self.formatString,self.seq_num,self.ack_num,self.datalen,self.chksum,self.ack_flag,self.syn_flag,self.fin_flag,self.mrws,self.data)
     
 class Window(object):
     def __init__(self,sequenceBit,serHost,serPort,mrws):
+        """Function fo initialize the window buffer
+
+        Args:
+            sequenceBit: the sequence number of the packet
+            serHost: server IP address
+            serPort: server Port number
+            mrws: window size
+
+        Returns:
+            null
+
+        """
         self.sequenceSize = pow(2,sequenceBit)
         #self.windowSize = pow(2,10)
         self.sendArray = [False] * self.sequenceSize
@@ -74,6 +141,15 @@ class Window(object):
                 print("retry connect...")
         return False
     def disConnect(self):
+        """Function fo initialize the disconnection with the server
+
+        Args:
+            null
+
+        Returns:
+            if the disconnecion has been successfully setup
+
+        """
         finPack = Packet("Bye",0,0,(0,0,1),self.rcvWindowSize)
         finMsg = finPack.pack()
         self.sock.settimeout(2)
@@ -89,10 +165,28 @@ class Window(object):
                 pass
         return False
     def setRevFile(self,fileName):
+        """Function fo set up the file to be written
+
+        Args:
+            filename: input file to be sent to the server
+
+        Returns:
+            null
+
+        """
         Name,fileType = fileName.split('.')
         self.rcvFile = Name + '-received'+'.'+fileType
         self.rcvWrite = open(self.rcvFile,'w')
     def sendPkt(self,data):
+        """Function fo send out the packet to the server
+
+        Args:
+            data: data to be included in the packet
+
+        Returns:
+            null
+
+        """
         pkt = Packet(data,self.lastSequence,0,(0,0,0),self.rcvWindowSize)
         pkt.time = time()
         pktMsg = pkt.pack()
@@ -108,7 +202,15 @@ class Window(object):
             except:
                 pass
     def moveToNext(self):
+        """Function fo move the window to the next unacked packet in the window buffer
 
+        Args:
+            null
+
+        Returns:
+            null
+
+        """
         while(self.sendArray[self.head]):
            
             print "write to file"
@@ -127,6 +229,15 @@ class Window(object):
             self.rcvWindowSize = self.rcvWindowSize + 1
             print "move window finished!"
     def rcvMsg(self,ackMsg):
+        """Function to acknowledge the packet from the server and move the window
+
+        Args:
+            ackMsg: received packet from server
+
+        Returns:
+            datareceived: decoded data length from ackMsg
+
+        """
         rcvPkt = self.decode(ackMsg)
         #print "decode finished"
         rcvChkSum = rcvPkt.makecheksum(rcvPkt.data,rcvPkt.datalen)
@@ -175,6 +286,15 @@ class Window(object):
                 
         return datareceived
     def finishTransfer(self):
+        """Function the terminate the transfer with server and refresh the window to the defaulted value
+
+        Args:
+            null
+
+        Returns:
+            if the termination is sucessful
+
+        """
         print "file " + self.rcvFile + "is downloaded"
         self.sendArray = [False] * self.sequenceSize
         self.pktArray = [False] * self.sequenceSize
@@ -202,6 +322,15 @@ class Window(object):
 
 
     def decode(self,response):
+        """Function the unpack the packet received from the server size 
+
+        Args:
+            null
+
+        Returns:
+            if the termination is sucessful
+
+        """
         string_format = "!HHHH???B"+ (str)(len(response) - 12) + "s"
         pack = struct.unpack(string_format, response)
         #print "pack:", pack
@@ -220,9 +349,28 @@ class Window(object):
         return packet
     #set timer in this method
     def windowFree(self):
+        """Function to check the window is free to receive further packet or not
+
+        Args:
+            null
+
+        Returns:
+            if the window is still free
+
+        """
         return self.lastSequence < self.end
 
     def checkTimeout(self,curTime):
+        """Function to check the timeout in the time array of unacked packet from window buffer, if the packet is timedout,
+           it will be resent immediately, with the new time stamp set up accordingly
+
+        Args:
+            curtime: current time stamp to be compared with the pre-allocated time stamp in each packet
+
+        Returns:
+            null
+
+        """
         print "Timeout check"
         print "head ", self.head
         if self.head < self.lastSequence:
@@ -265,6 +413,15 @@ TIMEOUT = 2 #seconds
 DATA_SIZE = 988
 lastAckTime = time()
 def istext(filename):
+    """helper Function of checkfile to check if the input file is a text file with valid acsii values
+
+    Args:
+        filename: the filename of the file to be checked
+
+    Returns:
+        if the input file is a valid text file
+
+    """
     try:
         f = open(filename)
         s = f.read(512)
@@ -287,6 +444,15 @@ def istext(filename):
     f.close()
     return True
 def checkFile(fileName):
+    """helper Function of checkfile to check if the input file is a text file with valid acsii values
+
+    Args:
+        filename: the filename of the file to be checked
+
+    Returns:
+        if the input file is a valid text file
+
+    """
     if not (istext(fileName)):
         print ("Binary File")
         return False
@@ -298,6 +464,17 @@ def checkFile(fileName):
     f.close()
     return True
 def connect(serHost,serPort,mrws):
+    """helper Function to start connection
+
+    Args:
+        serHost: the server address
+        serPost: the servere port number
+        mrws: max receive window size
+
+    Returns:
+        cliWin: Windows struct
+
+    """
     cliWin = Window(SEQUENCE_BIT,serHost,serPort,mrws)
     result = cliWin.cliConnect()
     if (result):
@@ -306,6 +483,16 @@ def connect(serHost,serPort,mrws):
         print ("fail to connect to server")
         sys.exit()
 def transfer(fileName,cliWin):
+    """helper Function to transfer file
+
+    Args:
+        filename: the filename to transfer
+        cliWin: client Window
+
+    Returns:
+        None
+
+    """
     f = open(fileName,'rb')
     data = f.read(DATA_SIZE)
     transferred = 0
@@ -354,9 +541,27 @@ def transfer(fileName,cliWin):
         cliWin.checkTimeout(curTime)
     return None
 def disconnect(cliWin):
+    """helper Function to  disconnect
+
+    Args:
+        cliWin: client Window
+
+    Returns:
+        True: successfully disconnect
+
+    """
     return cliWin.disConnect()
 
 def clientStart(argv):
+    """main loop function
+
+    Args:
+        argv: commandline input
+
+    Returns:
+        None
+
+    """
     if (len(argv) != 3):
         print("Invalid arguments")
         sys.exit()
